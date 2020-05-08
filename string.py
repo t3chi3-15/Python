a382989c1ef51c864c7754b5d0dc43276b257c9f
Pritesh Ghatawat <43085414+t3chi3-15@users.noreply.github.com>
2020-05-08 18:38:27 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,7 @@
+first = 'Pritesh'
+second = 'Ghatawat'
+third = 'Vice President'
+print(first, second)
+print(first, second, third)
+print(first, second, third, sep='-')
+print(first, second, third, sep='-', end='.')
