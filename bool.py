69af95787a1a96e3a782428c8982d0ed439797c0
Pritesh Ghatawat <43085414+t3chi3-15@users.noreply.github.com>
2020-05-08 18:43:31 +0530

    Create bool.py

---
+++ bool.py
@@ -0,0 +1,5 @@
+print(bool('True'))
+print(bool('False'))
+print(bool(''))
+print(bool(' '))
+print(bool('Hello, World!'))             #if empty strings it returns false otherwise true even a space will appear as true
